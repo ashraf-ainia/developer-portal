7e128354a4527c95f3daf582ed09d456cf8674ef
Ashraf Ainia <ashrafainia1995@gmail.com>
2025-10-02 11:18:35 +0300

    feat: initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+    rules: {
+      // You can customize or disable rules here
+      "react/no-unescaped-entities": "error", // Enforce the rule
+      // or "react/no-unescaped-entities": "off" to disable it
+    },
+  },
+];
+
+export default eslintConfig;
